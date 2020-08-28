d55b32c91945b302d33e0a03b2186efc1587c549
Beta-23 <alfred.dominguez@gmail.com>
2020-08-27 21:10:52 -0700

    Setup API

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
 // Unsplash API
 const count = 10;
-const apiKey = '';
+const apiKey = 'config.API_KEY';
 const apiUrl = `https://api.unsplash.com/photos/random/?client_id=${apiKey}&count=${count}`;
