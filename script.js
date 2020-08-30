9771fa53aad50033b7932269588c436896ab24f2
Beta-23 <alfred.dominguez@gmail.com>
2020-08-29 22:42:18 -0700

    Create the link to api and displayPhotos

--- script.js
+++ script.js
@@ -1,4 +1,44 @@
+const imageContainer = document.getElementById('image-container');
+const loader = document.getElementById('loader');
+
+let photosArray = [];
+
 // Unsplash API
-const count = 10;
-const apiKey = 'config.API_KEY';
-const apiUrl = `https://api.unsplash.com/photos/random/?client_id=${apiKey}&count=${count}`;
+const count = 30;
+const apiKey = 'API_KEY';
+const apiUrl = `https://api.unsplash.com/photos/random?client_id=${apiKey}&count=${count}`;
+
+// Create Elements For Links & Photos, Add to DOM
+function displayPhotos () {
+	// Run function for each object in photosArray
+	photosArray.forEach((photo) => {
+		// Create <a> to link to full photo
+		const item = document.createElement('a');
+		setAttributes(item, {
+			href: photo.links.html,
+			target: '_blank'
+		});
+		// Create <img> for photo
+		const img = document.createElement('img');
+		setAttributes(img, {
+			src: photo.urls.regular,
+			alt: photo.alt_description,
+			title: photo.alt_description
+		});
+
+		// Put <img> inside <a>, then put both inside imageContainer Element
+		item.appendChild(img);
+		imageContainer.appendChild(item);
+	});
+}
+
+// Get photos from Unsplash API
+async function getUnsplashPhotos () {
+	try {
+		const response = await fetch(apiUrl);
+		photosArray = await response.json();
+		displayPhotos();
+	} catch (error) {}
+}
+
+getUnsplashPhotos();
